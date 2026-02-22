b18c76e0ea0fb76ecefa3c119fcdb91a484ec3b2
Madjak3 <159061670+Madjak3@users.noreply.github.com>
2026-02-21 20:53:04 -0500

    Finished Conversation System
    Fully finished the conversation system and demo scene.

--- Assets/Scripts/ConversationStatRegister.cs
+++ Assets/Scripts/ConversationStatRegister.cs
@@ -1,10 +1,28 @@
+using JetBrains.Annotations;
 using PixelCrushers.DialogueSystem;
 using PixelCrushers.DialogueSystem.UnityGUI;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ConversationStatRegister : MonoBehaviour
 {
-    public CharacterStats characterStats;
+    //Referencing GameObjects
+    private CharacterStats characterStats;
+    public GameObject athleticsTrigger;
+    public GameObject sleightOfHandTrigger;
+    public GameObject arcanaTrigger;
+    public GameObject historyTrigger;
+    public GameObject investigationTrigger;
+    public GameObject religionTrigger;
+    public GameObject insightTrigger;
+    public GameObject deceptionTrigger;
+    public GameObject persuasionTrigger;
+    public GameObject performanceTrigger;
+    public GameObject intimidationTrigger;
+
+    //int used for the dice roll
+    int diceRollNumber;
 
     string currentAthletics = "0";
     string currentSOH = "0";
@@ -20,25 +38,21 @@ public class ConversationStatRegister : MonoBehaviour
 
     void Start()
     {
+        //Grabbing the Character Stats script in order to reference it
         characterStats = Object.FindFirstObjectByType<CharacterStats>();
-        /*
-        Debug.Log(characterStats.athletics);
-        Debug.Log(characterStats.sleightOfHand);
-        Debug.Log(characterStats.arcana);
-        Debug.Log(characterStats.history);
-        Debug.Log(characterStats.investigation);
-        Debug.Log(characterStats.religion);
-        Debug.Log(characterStats.insight);
-        Debug.Log(characterStats.deception);
-        Debug.Log(characterStats.persuasion);
-        Debug.Log(characterStats.performance);
-        Debug.Log(characterStats.intimidation);
-        */
     }
 
     
     void Update()
     {
+        //Updates the players stats within this script ever 10 second via coroutine
+        StartCoroutine(UpdatePlayerStats());
+    }
+
+    IEnumerator UpdatePlayerStats()
+    {
+        yield return new WaitForSeconds(10);
+
         currentAthletics = characterStats.athletics.ToString();
         currentSOH = characterStats.sleightOfHand.ToString();
         currentArcana = characterStats.arcana.ToString();
@@ -52,20 +66,293 @@ public class ConversationStatRegister : MonoBehaviour
         currentIntimidation = characterStats.intimidation.ToString();
     }
 
-    public bool HasAthletics(string requiredAthletics)
+    //Function for generating dice roll
+    private void GenerateDiceRoll()
     {
-        return currentAthletics == requiredAthletics;
+        diceRollNumber = Random.Range(1, 20);
+    }
+
+    bool HasAthletics(string requiredAthletics)
+    {
+        if (athleticsTrigger.activeSelf)
+        { 
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.athletics)
+            {
+                requiredAthletics = "1";
+                return true;
+            }
+            else
+            {
+                requiredAthletics = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasSOH(string requiredSOH)
+    {
+        if (sleightOfHandTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.sleightOfHand)
+            {
+                requiredSOH = "1";
+                return true;
+            }
+            else
+            {
+                requiredSOH = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasArcana(string requiredArcana)
+    {
+        if (arcanaTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.arcana)
+            {
+                requiredArcana = "1";
+                return true;
+            }
+            else
+            {
+                requiredArcana = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasHistory(string requiredHistory)
+    {
+        if (historyTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.history)
+            {
+                requiredHistory = "1";
+                return true;
+            }
+            else
+            {
+                requiredHistory = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasInvestigation(string requiredInvestigation)
+    {
+        if (investigationTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.investigation)
+            {
+                requiredInvestigation = "1";
+                return true;
+            }
+            else
+            {
+                requiredInvestigation = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasReligion(string requiredReligion)
+    {
+        if (religionTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.religion)
+            {
+                requiredReligion = "1";
+                return true;
+            }
+            else
+            {
+                requiredReligion = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    bool HasInsight(string requiredInsight)
+    {
+        if (insightTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.insight)
+            {
+                requiredInsight = "1";
+                return true;
+            }
+            else
+            {
+                requiredInsight = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasDeception(string requiredDeception)
+    {
+        if (deceptionTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.deception)
+            {
+                requiredDeception = "1";
+                return true;
+            }
+            else
+            {
+                requiredDeception = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasPersuasion(string requiredPersuasion)
+    {
+        if (persuasionTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.persuasion)
+            {
+                requiredPersuasion = "1";
+                return true;
+            }
+            else
+            {
+                requiredPersuasion = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasPerformance(string requiredPerformance)
+    {
+        if (performanceTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.performance)
+            {
+                requiredPerformance = "1";
+                return true;
+            }
+            else
+            {
+                requiredPerformance = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool HasIntimidation(string requiredIntimidation)
+    {
+        if (intimidationTrigger.activeSelf)
+        {
+            GenerateDiceRoll();
+            if (diceRollNumber <= characterStats.intimidation)
+            {
+                requiredIntimidation = "1";
+                return true;
+            }
+            else
+            {
+                requiredIntimidation = "0";
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
     #region Register with Lua
     void OnEnable()
     {
+        //Registering the bool functions with the Lua code so they can be used by the dialogue system
+        //New entries can use the following template below
+
+        //Lua.RegisterFunction("EnterFunctionNameHere", this, SymbolExtensions.GetMethodInfo(() => EnterFunctionNameHere(string.Empty)));
+
         Lua.RegisterFunction("HasAthletics", this, SymbolExtensions.GetMethodInfo(() => HasAthletics(string.Empty)));
+        Lua.RegisterFunction("HasSOH", this, SymbolExtensions.GetMethodInfo(() => HasSOH(string.Empty)));
+        Lua.RegisterFunction("HasArcana", this, SymbolExtensions.GetMethodInfo(() => HasArcana(string.Empty)));
+        Lua.RegisterFunction("HasHistory", this, SymbolExtensions.GetMethodInfo(() => HasHistory(string.Empty)));
+        Lua.RegisterFunction("HasInvestigation", this, SymbolExtensions.GetMethodInfo(() => HasInvestigation(string.Empty)));
+        Lua.RegisterFunction("HasReligion", this, SymbolExtensions.GetMethodInfo(() => HasReligion(string.Empty)));
+        Lua.RegisterFunction("HasInsight", this, SymbolExtensions.GetMethodInfo(() => HasInsight(string.Empty)));
+        Lua.RegisterFunction("HasDeception", this, SymbolExtensions.GetMethodInfo(() => HasDeception(string.Empty)));
+        Lua.RegisterFunction("HasPersuasion", this, SymbolExtensions.GetMethodInfo(() => HasPersuasion(string.Empty)));
+        Lua.RegisterFunction("HasPerformance", this, SymbolExtensions.GetMethodInfo(() => HasPerformance(string.Empty)));
+        Lua.RegisterFunction("HasIntimidation", this, SymbolExtensions.GetMethodInfo(() => HasIntimidation(string.Empty)));
     }
 
     void OnDisable()
     {
-        Lua.UnregisterFunction(nameof(HasAthletics));
+        //Disables the bool functions in Lua since they are not on a persistent object
+        //New entries can use the following template below
+
+        //Lua.UnregisterFunction("EnterFunctionNameHere");
+
+        Lua.UnregisterFunction("HasAthletics");
+        Lua.UnregisterFunction("HasSOH");
+        Lua.UnregisterFunction("HasArcana");
+        Lua.UnregisterFunction("HasHistory");
+        Lua.UnregisterFunction("HasInvestigation");
+        Lua.UnregisterFunction("HasReligion");
+        Lua.UnregisterFunction("HasInsight");
+        Lua.UnregisterFunction("HasDeception");
+        Lua.UnregisterFunction("HasPersuasion");
+        Lua.UnregisterFunction("HasPerformance");
+        Lua.UnregisterFunction("HasIntimidation");
     }
     #endregion
 }
